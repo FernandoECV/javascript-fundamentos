cb964a4171700c252bb176dd73708bf83165d17a
Fernando Santos <fernandop.santossa@gmail.com>
2022-10-17 16:51:48 -0300

    Declaração de variaveis

--- script.js
+++ script.js
@@ -7,3 +7,14 @@
 
 // console.log(Symbol('Programando'));
 
+let idade = 28,
+    nome = 'João',
+    texto = '';
+
+const PI = 3.14;
+   
+// texto = 'A idade de '+ nome + ' é ' + idade;
+texto = `A idade de ${nome} é ${idade}`;
+
+console.log(texto);
+console.log(idade);
