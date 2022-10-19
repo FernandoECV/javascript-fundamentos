2fc345bd7e442641e586342caa71a9111c38b62a
fernandoecv <fernandop.santossa@gmail.com>
2022-10-19 11:48:53 -0300

    Condições Javacript

--- script.js
+++ script.js
@@ -7,14 +7,40 @@
 
 // console.log(Symbol('Programando'));
 
-let idade = 28,
-    nome = 'João',
-    texto = '';
+// let idade = 28,
+//     nome = 'João',
+//     texto = '';
 
-const PI = 3.14;
-   
-// texto = 'A idade de '+ nome + ' é ' + idade;
-texto = `A idade de ${nome} é ${idade}`;
+// const PI = 3.14;
 
-console.log(texto);
-console.log(idade);
+// // texto = 'A idade de '+ nome + ' é ' + idade;
+// texto = `A idade de ${nome} é ${idade}`;
+
+// console.log(texto);
+// console.log(idade);
+
+let hora = 19,
+    mensagem = '';
+
+if (hora < 12) {
+
+    mensagem = 'Bom dia!';
+
+} else if (hora >= 12 && hora < 18) {
+
+    mensagem = 'Boa Tarde!';
+
+} else {
+
+    mensagem = 'Boa Noite!';
+}
+
+// if(hora >= 12 && hora < 18) {
+//     mensagem = 'Boa tarde!';
+// }
+
+// if(hora >= 18 ) {
+//     mensagem = 'Boa Noite!';
+// }
+
+console.log(mensagem);
